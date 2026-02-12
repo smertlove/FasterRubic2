8949aada6cd0e90ade87b591c16cc0a0de61ad2c
Kirill Soloshenko <k.soloshenko1@mts.ai>
2026-02-13 02:04:44 +0300

    new metrics

--- src/metrics.py
+++ src/metrics.py
@@ -7,7 +7,7 @@ import pymorphy3 as pm
 from functools import lru_cache
 
 
-class Lemmatizer:
+class _Lemmatizer:
     morph = pm.MorphAnalyzer()
 
     @classmethod
@@ -16,48 +16,48 @@ class Lemmatizer:
         return cls.morph.parse(word)[0].normal_form
 
 
-def __preproc(word: str, normalize=False):
+def _preproc(word: str, normalize=False):
 
     result = word.lower().replace("ё", "е")
 
     if normalize:
-        result = Lemmatizer.lemmatize(result)
+        result = _Lemmatizer.lemmatize(result)
 
     return result
 
 
-def lemmatization_accuracy(targets: list[str], preds: list[str], normalize=False):
+def _lemmatization_accuracy(targets: list[str], preds: list[str], normalize=False) -> list[int]:
 
     assert len(targets) == len(preds)
 
-    matches = 0
-    tot = len(targets)
+    accs = []
 
     for target, pred in zip(targets, preds):
 
-        matches += __preproc(target, normalize=normalize) == __preproc(
+        cur_acc = _preproc(target, normalize=normalize) == _preproc(
             pred, normalize=normalize
         )
 
-    return matches / tot
+        accs.append(cur_acc)
+
+    return accs
 
 
-def lemmatization_cer(targets: list[str], preds: list[str]):
+def _lemmatization_cer(targets: list[str], preds: list[str]) -> list[float]:
 
     assert len(targets) == len(preds)
 
-    cers = []
+    cers = [
+        cer(
+            target,
+            pred,
+            reference_transform=_preproc,
+            hypothesis_transform=_preproc,
+        )
+        for target, pred in zip(targets, preds)
+    ]
 
-    for target, pred in zip(targets, preds):
-
-        preproc_target = __preproc(target)
-        preproc_pred = __preproc(pred)
-
-        if preproc_target != preproc_pred:
-
-            cers.append(cer(preproc_target, preproc_pred))
-
-    return np.mean(cers)
+    return cers
 
 
 def preds_time_tps_lacc(predict_function, df):
@@ -73,7 +73,7 @@ def preds_time_tps_lacc(predict_function, df):
 
     tps = len(inputs) / elapsed_time
 
-    lacc = lemmatization_accuracy(targets, preds)
+    lacc = np.mean(_lemmatization_accuracy(targets, preds))
 
     return preds, elapsed_time, tps, lacc
 
@@ -96,7 +96,7 @@ def _get_freq_class(rank: str):
     return "10001-n"
 
 
-def _get_freq_dfs(df, filter_irrelevant_=True):
+def _add_freq_class(df, filter_irrelevant_=True):
     df_copy = df.copy()
 
     if filter_irrelevant_:
@@ -104,45 +104,32 @@ def _get_freq_dfs(df, filter_irrelevant_=True):
 
     df_copy["freq_class"] = df_copy["freq_rank"].map(_get_freq_class)
 
-    freq_dfs = {
-        "1-100": df_copy[df_copy["freq_class"] == "1-100"],
-        "101-1000": df_copy[df_copy["freq_class"] == "101-1000"],
-        "1001-10000": df_copy[df_copy["freq_class"] == "1001-10000"],
-        "10001-n": df_copy[df_copy["freq_class"] == "10001-n"],
-        "all": df_copy,
-    }
+    return df_copy
 
-    return freq_dfs
+    # freq_dfs = {
+    #     "1-100": df_copy[df_copy["freq_class"] == "1-100"],
+    #     "101-1000": df_copy[df_copy["freq_class"] == "101-1000"],
+    #     "1001-10000": df_copy[df_copy["freq_class"] == "1001-10000"],
+    #     "10001-n": df_copy[df_copy["freq_class"] == "10001-n"],
+    #     "all": df_copy,
+    # }
+
+    # return freq_dfs
 
 
 def lacc_cer_by_freq_classes(
-    predict_function, df, filter_irrelevant_=True, return_preds=False
+    predict_function,
+    df,
+    filter_irrelevant_=True,
 ):
 
-    freq_dfs = _get_freq_dfs(df, filter_irrelevant_=filter_irrelevant_)
+    freq_df = _add_freq_class(df, filter_irrelevant_=filter_irrelevant_)
 
-    result = dict()
+    inputs, targets = prep_inpts_targets(freq_df)
+    preds = predict_function(inputs)
 
-    if return_preds:
-        result_preds = dict()
-        result_targets = dict()
+    freq_df["lAcc"] = _lemmatization_accuracy(targets, preds)
+    freq_df["lAcc (norm)"] = _lemmatization_accuracy(targets, preds, normalize=True)
+    freq_df["CER"] = _lemmatization_cer(targets, preds)
 
-    for freq_name, freq_df in freq_dfs.items():
-        inputs, targets = prep_inpts_targets(freq_df)
-
-        preds = predict_function(inputs)
-
-        lacc = lemmatization_accuracy(targets, preds)
-        cer_ = lemmatization_cer(targets, preds)
-        lacc_norm = lemmatization_accuracy(targets, preds, normalize=True)
-
-        result[freq_name] = (lacc, cer_, lacc_norm)
-
-        if return_preds:
-            result_preds[freq_name] = preds
-            result_targets[freq_name] = targets
-
-    if return_preds:
-        return result, result_preds, result_targets
-
-    return result
+    return freq_df
